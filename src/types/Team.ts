3edf2360ec29734e8377a06e57a9e1216776c82d
Andree Jimenez <andree_jimenez@outlook.com>
2025-05-22 20:18:54 -0600

    Segmentate code types

---
+++ src/types/Team.ts
@@ -0,0 +1,15 @@
+export interface Team {
+  id: number
+  name: string
+  badge: string
+  wins: number
+  losses: number
+  draws: number
+  points: number
+  goalsFor: number
+  goalsAgainst: number
+  description: string
+  stadium: string
+  foundedYear: number
+  coach: string
+}
